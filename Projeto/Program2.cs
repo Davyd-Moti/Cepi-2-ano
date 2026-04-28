05b1c1039b03a9457ac9595bd0023af1470b9ab6
Davyd Nickolas <davydnick@gmail.com>
2026-04-28 11:40:24 -0300

    Create Program2.cs

---
+++ Projeto/Program2.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_aura_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa t1 = new Pessoa();
+            Pessoa t2 = new Pessoa();
+
+            Console.WriteLine("Dados da primeira pessoa: ");
+            Console.WriteLine("Nome: ");
+            t1.nome = Console.ReadLine();
+            Console.WriteLine("Idade: ");
+            t1.idade = int.Parse(Console.ReadLine());
+
+			Console.WriteLine("Dados da segunda pessoa: ");
+			Console.WriteLine("Nome: ");
+			t2.nome = Console.ReadLine();
+			Console.WriteLine("Idade: ");
+			t2.idade = int.Parse(Console.ReadLine());
+
+            if 
+                (t1.idade > t2.idade)
+            {
+                Console.WriteLine("a pessoa mais velha é " + t1.nome);
+            }
+            else 
+            {
+                Console.WriteLine("a pessoa mais velha é " + t2.nome);
+            }
+		}
+    }
+}
