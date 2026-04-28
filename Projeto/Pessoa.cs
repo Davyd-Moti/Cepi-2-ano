ab00c698de99f3455710aa5dde97886db1a27373
Davyd Nickolas <davydnick@gmail.com>
2026-04-28 11:40:44 -0300

    Create Pessoa.cs

---
+++ Projeto/Pessoa.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_aura_2
+{
+    internal class Pessoa
+    {
+        public string nome;
+        public int idade;
+
+
+    }
+}
